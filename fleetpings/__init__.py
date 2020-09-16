2e50d74db769cbdea8801c07abdf306951e8c2ae
Peter Pfeufer <info@ppfeufer.de>
2020-09-16 20:48:48 +0200

    pushing v2.1.0

--- fleetpings/__init__.py
+++ fleetpings/__init__.py
@@ -8,5 +8,5 @@ from django.utils.translation import gettext_lazy as _
 
 default_app_config = "fleetpings.apps.AaFleetpingsConfig"
 
-__version__ = "2.0.0"
+__version__ = "2.1.0"
 __title__ = _("Fleet Pings")

124cdd5d7b981b38f2e72e3a3659edc6198bd2a8
Peter Pfeufer <info@ppfeufer.de>
2020-09-16 20:47:03 +0200

    link label/name translated

--- fleetpings/__init__.py
+++ fleetpings/__init__.py
@@ -4,7 +4,9 @@
 a couple of variable to use throughout the app
 """
 
+from django.utils.translation import gettext_lazy as _
+
 default_app_config = "fleetpings.apps.AaFleetpingsConfig"
 
 __version__ = "2.0.0"
-__title__ = "Fleet Pings"
+__title__ = _("Fleet Pings")

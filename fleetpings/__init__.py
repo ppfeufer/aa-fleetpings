d39b25207db53893aef37c1cb35bb08c44677499
Peter Pfeufer <info@ppfeufer.de>
2020-11-08 00:31:00 +0100

    version raised

--- fleetpings/__init__.py
+++ fleetpings/__init__.py
@@ -6,5 +6,5 @@ a couple of variable to use throughout the app
 
 default_app_config: str = "fleetpings.apps.AaFleetpingsConfig"
 
-__version__ = "2.4.1"
+__version__ = "2.4.2"
 __title__ = "Fleet Pings"

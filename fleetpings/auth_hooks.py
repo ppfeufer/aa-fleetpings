a2a7c9b6dda621aa4c56489d6433470896ff1a34
Peter Pfeufer <info@ppfeufer.de>
2021-05-02 00:26:44 +0200

    black formatting

--- fleetpings/auth_hooks.py
+++ fleetpings/auth_hooks.py
@@ -11,7 +11,7 @@ from fleetpings import __title__, urls
 
 
 class AaFleetpingsMenuItem(MenuItemHook):  # pylint: disable=too-few-public-methods
-    """ This class ensures only authorized users will see the menu entry """
+    """This class ensures only authorized users will see the menu entry"""
 
     def __init__(self):
         # setup menu entry for sidebar

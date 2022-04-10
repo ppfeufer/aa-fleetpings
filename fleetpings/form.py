9489d8ac4982c4217b477799991e08cfaa04cc0f
Peter Pfeufer <info@ppfeufer.de>
2022-04-10 19:36:19 +0200

    Form rebuild with form object

--- fleetpings/form.py
+++ fleetpings/form.py
@@ -4,12 +4,24 @@ Form declarations
 
 # Django
 from django import forms
+from django.utils.translation import gettext_lazy as _
 
 # AA Fleet Pings
+from fleetpings.app_settings import timezones_installed
 from fleetpings.models import FleetType
 
 # from django.core.handlers.wsgi import WSGIRequest
 # from django.db.models import Q
+
+
+def _get_timezones_module_hint_text() -> str:
+    if timezones_installed():
+        return _(
+            " Timezones module is installed. Link to time zone conversion will be "
+            "added automatically if you set a date and time here."
+        )
+
+    return ""
 
 
 # def _get_ping_targets(request: WSGIRequest):
@@ -62,5 +74,102 @@ class FleetPingForm(forms.Form):
     Fleet Ping Form
     """
 
-    ping_target = forms.CharField(label="Ping Target", widget=forms.Select(choices={}))
-    # ping_target = forms.MultipleChoiceField(choices=_get_ping_targets(_user))
+    ping_target = forms.CharField(
+        label=_("Ping Target"), widget=forms.Select(choices={})
+    )
+    pre_ping = forms.BooleanField(
+        initial=False,
+        required=False,
+        label=_("Pre-Ping"),
+        help_text=_("Mark this checkbox if this should be a pre-ping."),
+    )
+    ping_channel = forms.CharField(
+        label=_("Ping To"),
+        widget=forms.Select(choices={}),
+        help_text=_("Select a channel to ping automatically"),
+    )
+    fleet_type = forms.CharField(
+        label=_("Fleet Type"),
+        widget=forms.Select(choices={}),
+        help_text=_("Select a fleet type"),
+    )
+    fleet_commander = forms.CharField(
+        required=True,
+        label=_("FC Name"),
+        max_length=254,
+        widget=forms.TextInput(attrs={"placeholder": _("Who is the FC?")}),
+    )
+    fleet_name = forms.CharField(
+        required=True,
+        label=_("Fleet Name"),
+        max_length=254,
+        widget=forms.TextInput(
+            attrs={"placeholder": _("What is the fleet name in fleet finder?")}
+        ),
+    )
+    formup_location = forms.CharField(
+        label=_("Formup Location"), widget=forms.Select(choices={})
+    )
+    formup_time = forms.CharField(
+        required=True,
+        label=_("Formup Time"),
+        max_length=254,
+        help_text=_(
+            "To enable this field, either make it a Pre-Ping (checkbox above) or "
+            "uncheck &quot;Formup NOW&quot; (checkbox below)."
+        )
+        + _get_timezones_module_hint_text(),
+    )
+    formup_now = forms.BooleanField(
+        initial=True,
+        required=False,
+        label=_("Formup NOW"),
+        help_text=_(
+            "If this checkbox is active, formup time will be set to &quot;NOW&quot; "
+            "and the time in the field above (if any is set) will be ignored."
+        ),
+    )
+    fleet_comms = forms.CharField(
+        label=_("Fleet Comms"), widget=forms.Select(choices={})
+    )
+    fleet_doctrine = forms.CharField(
+        label=_("Doctrine"), widget=forms.Select(choices={})
+    )
+    srp = forms.BooleanField(
+        initial=False,
+        required=False,
+        label=_("SRP"),
+        help_text=_("Is this fleet covered by SRP?"),
+    )
+    srp_link = forms.BooleanField(
+        initial=False,
+        required=False,
+        label=_("Create SRP Link"),
+        help_text=_(
+            "If this checkbox is active, a SRP link specific for this fleet will be "
+            "created.<br>Leave blank if unsure."
+        ),
+    )
+    additional_information = forms.CharField(
+        required=False,
+        label=_("Additional Information"),
+        widget=forms.Textarea(
+            attrs={
+                "rows": 10,
+                "cols": 20,
+                "input_type": "textarea",
+                "placeholder": _(
+                    "Feel free to add some more information about the fleet ..."
+                ),
+            }
+        ),
+    )
+    optimer = forms.BooleanField(
+        initial=False,
+        required=False,
+        label=_("Create Optime"),
+        help_text=_(
+            "If this checkbox is active, a fleet operations timer for this pre-ping "
+            "will be created."
+        ),
+    )

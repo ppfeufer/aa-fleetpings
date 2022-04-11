f00696e0b2f59efecc7aa123540f4ed9664dedba
Peter Pfeufer <info@ppfeufer.de>
2022-04-11 13:51:58 +0200

    Some access tests added

--- testauth/settings.py
+++ testauth/settings.py
@@ -258,7 +258,7 @@ SITE_NAME = "testauth"
 DEBUG = False
 
 # Add any additional apps to this list.
-INSTALLED_APPS += ["eveuniverse", "fleetpings"]
+INSTALLED_APPS += ["eveuniverse", "fleetpings", "aasrp", "fittings", "timezones"]
 
 # Register an application at https://developers.eveonline.com for Authentication
 # & API Access and fill out these settings. Be sure to set the callback URL

cc7c1ef7f8818f57bb17437224a7f395b2a65ef8
Peter Pfeufer <info@ppfeufer.de>
2022-04-16 14:30:17 +0200

    JS code cleaned up

--- fleetpings/static/fleetpings/js/fleetpings.js
+++ fleetpings/static/fleetpings/js/fleetpings.js
@@ -1,4 +1,4 @@
-/* global fleetpingsSettings, fleetpingsTranslations, ClipboardJS, console */
+/* global fleetpingsSettings, fleetpingsTranslations, ClipboardJS */
 
 $(document).ready(() => {
     'use strict';
@@ -41,7 +41,7 @@ $(document).ready(() => {
         $.ajax({
             url: fleetpingsSettings.url.pingTargets,
             success: (data) => {
-                $("#id_ping_target").html(data);
+                $('#id_ping_target').html(data);
             }
         });
     };
@@ -53,7 +53,7 @@ $(document).ready(() => {
         $.ajax({
             url: fleetpingsSettings.url.pingWebhooks,
             success: (data) => {
-                $("#id_ping_channel").html(data);
+                $('#id_ping_channel').html(data);
             }
         });
     };
@@ -77,7 +77,7 @@ $(document).ready(() => {
         $.ajax({
             url: fleetpingsSettings.url.formupLocations,
             success: (data) => {
-                $("#id_formup_location").after(data);
+                $('#id_formup_location').after(data);
             }
         });
     };
@@ -89,7 +89,7 @@ $(document).ready(() => {
         $.ajax({
             url: fleetpingsSettings.url.fleetComms,
             success: (data) => {
-                $("#id_fleet_comms").after(data);
+                $('#id_fleet_comms').after(data);
             }
         });
     };
@@ -101,24 +101,9 @@ $(document).ready(() => {
         $.ajax({
             url: fleetpingsSettings.url.fleetDoctrines,
             success: (data) => {
-                $("#id_fleet_doctrine").after(data);
+                $('#id_fleet_doctrine').after(data);
             }
         });
-    };
-
-    /**
-     * Convert line breaks into <br>
-     *
-     * @param {string} string
-     * @param {boolean} isXhtml
-     */
-    const nl2br = (string, isXhtml) => {
-        const breakTag = isXhtml || typeof isXhtml === 'undefined' ? '<br />' : '<br>';
-
-        return (string + '').replace(
-            /([^>\r\n]?)(\r\n|\n\r|\r|\n)/g,
-            '$1' + breakTag + '$2'
-        );
     };
 
     /**
@@ -221,59 +206,6 @@ $(document).ready(() => {
     };
 
     /**
-     * Send an embedded message to a Discord webhook
-     *
-     * @param {string} webhookUrl Discord webhook URL
-     * @param {string} content Message to send to Discord
-     * @param {object} embeds Embedded content » https://discohook.org/ - https://leovoel.github.io/embed-visualizer/
-     */
-    // const sendEmbeddedDiscordPing = (webhookUrl, content, embeds) => {
-    //     const request = new XMLHttpRequest();
-    //
-    //     request.open('POST', webhookUrl);
-    //     request.setRequestHeader('Content-type', 'application/json');
-    //
-    //     const params = {
-    //         username: '',
-    //         avatar_url: '',
-    //         content: content,
-    //         embeds: [embeds]
-    //     };
-    //
-    //     request.send(JSON.stringify(params));
-    // };
-
-    /**
-     * Send a message to a Discord webhook
-     *
-     * @param {string} webhookUrl Discord webhook URL
-     * @param {string} pingText Message to send to Discord
-     */
-    // const sendDiscordPing = (webhookUrl, pingText) => {
-    //     const request = new XMLHttpRequest();
-    //
-    //     request.open('POST', webhookUrl);
-    //     request.setRequestHeader('Content-type', 'application/json');
-    //
-    //     const params = {
-    //         username: '',
-    //         avatar_url: '',
-    //         content: pingText
-    //     };
-    //
-    //     request.send(JSON.stringify(params));
-    // };
-
-    /**
-     * Convert hex color code in something Discord can handle
-     *
-     * @param {string} hexValue
-     */
-    // const hexToDecimal = (hexValue) => {
-    //     return parseInt(hexValue.replace('#', ''), 16);
-    // };
-
-    /**
      * Get the timestamp for the formup time
      *
      * @param {string} formupTime
@@ -284,300 +216,6 @@ $(document).ready(() => {
 
         return (formupDateTime.getTime() - formupDateTime.getTimezoneOffset() * 60 * 1000) / 1000;
     };
-
-    /**
-     * Convert the datepicker info into a URL that the aa-timezones module understands
-     *
-     * @param {string} formupTime
-     */
-    // const getTimezonesUrl = (formupTime) => {
-    //     const formupTimestamp = getFormupTimestamp(formupTime);
-    //
-    //     return fleetpingsSettings.url.siteUrl + fleetpingsSettings.url.timezonesUrl + formupTimestamp + '/';
-    // };
-
-    /**
-     * Create the ping text
-     *
-     * @param {string} fleetSrpCode SRP code for the fleet, if available
-     */
-//     const generateFleetPing = (fleetSrpCode) => {
-//         const pingTargetSelected = $('option:selected', selectPingTarget);
-//         const pingTarget = sanitizeInput(pingTargetSelected.val());
-//         const pingTargetText = sanitizeInput(pingTargetSelected.text());
-//         const pingChannelSelected = $('option:selected', selectPingChannel);
-//         const webhookType = sanitizeInput(pingChannelSelected.data('webhook-type'));
-//         const webhookEmbedPing = sanitizeInput(pingChannelSelected.data('webhook-embed'));
-//         const fleetTypeSelected = $('option:selected', selectFleetType);
-//         const fleetType = sanitizeInput(fleetTypeSelected.val());
-//         const webhookEmbedColor = sanitizeInput(fleetTypeSelected.data('embed-color'));
-//         const fcName = sanitizeInput(inputFcName.val());
-//         const fleetName = sanitizeInput(inputFleetName.val());
-//         const formupLocation = sanitizeInput(inputFormupLocation.val());
-//         const formupTime = sanitizeInput(inputFormupTime.val());
-//         const fleetComms = sanitizeInput(inputFleetComms.val());
-//         const fleetDoctrine = sanitizeInput(inputFleetDoctrine.val());
-//         const fleetSrp = sanitizeInput($('option:selected', selectFleetSrp).val());
-//         const additionalInformation = sanitizeInput(textAdditionalInformation.val());
-//
-//         // Let's see if we can find a doctrine link
-//         let fleetDoctrineLink = null;
-//         if (fleetDoctrine !== '') {
-//             const selectedLink = $('#fleetDoctrineList [value="' + escapeInput(fleetDoctrine, false) + '"]').data('doctrine-url');
-//
-//             if ('undefined' !== selectedLink && selectedLink !== '') {
-//                 // Houston, we have a link!
-//                 fleetDoctrineLink = selectedLink;
-//             }
-//         }
-//
-//         // Ping webhooks, if configured
-//         let webhookUrl = '';
-//
-//         if (selectPingChannel.length) {
-//             webhookUrl = sanitizeInput(pingChannelSelected.val());
-//         }
-//
-//         $('.aa-fleetpings-no-ping').hide('fast');
-//         $('.aa-fleetpings-ping').show('fast');
-//
-//         let webhookPingTextHeader = '';
-//         let webhookPingTextContent = '';
-//         let webhookPingTextFooter = '';
-//         let pingText = '';
-//
-//         // Determine pingTarget and pingTargetText
-//         let discordPingTarget = '';
-//         let webhookPingTarget = '';
-//
-//         if (pingTarget !== '') {
-//             // pingTarget
-//             if (pingTarget.indexOf('@') > -1) {
-//                 webhookPingTarget = pingTarget;
-//                 discordPingTarget = pingTarget;
-//             } else {
-//                 webhookPingTarget = '<@&' + pingTarget + '>';
-//
-//                 // pingTargetText
-//                 if (pingTargetText.indexOf('@') > -1) {
-//                     discordPingTarget = pingTargetText;
-//                 } else {
-//                     discordPingTarget = '@' + pingTargetText;
-//                 }
-//             }
-//
-//             // Separator
-//             pingText += ' :: ';
-//         }
-//
-//         // Fleet announcement
-//         pingText += '**';
-//
-//         // Check if it's a pre-ping or not
-//         if (checkboxPrePing.is(':checked')) {
-//             pingText += '### PRE PING ###';
-//             webhookPingTextHeader += '### PRE PING ###';
-//
-//             if (fleetType !== '') {
-//                 pingText += ' / (Upcoming)  ' + fleetType + ' Fleet';
-//                 webhookPingTextHeader += ' / (Upcoming) ' + fleetType + ' Fleet';
-//             }
-//         } else {
-//             if (fleetType !== '') {
-//                 pingText += fleetType + ' ';
-//                 webhookPingTextHeader += fleetType + ' ';
-//             }
-//
-//             pingText += 'Fleet is up';
-//             webhookPingTextHeader += 'Fleet is up';
-//         }
-//
-//         // Add fcName if we have one
-//         if (fcName !== '') {
-//             pingText += ' under ' + fcName;
-//             webhookPingTextHeader += ' under ' + fcName;
-//         }
-//
-//         pingText += '**' + '\n';
-//
-//         // Check if FC name is available
-//         if (fcName !== '') {
-//             pingText += '\n' + '**FC:** ' + fcName;
-//             webhookPingTextContent += '\n' + '**FC:** ' + fcName;
-//         }
-//
-//         // Check if fleet name is available
-//         if (fleetName !== '') {
-//             pingText += '\n' + '**Fleet Name:** ' + fleetName;
-//             webhookPingTextContent += '\n' + '**Fleet Name:** ' + fleetName;
-//         }
-//
-//         // Check if form-up location is available
-//         if (formupLocation !== '') {
-//             pingText += '\n' + '**Formup Location:** ' + formupLocation;
-//             webhookPingTextContent += '\n' + '**Formup Location:** ' + formupLocation;
-//         }
-//
-//         // Check if form-up time is available
-//         if (checkboxFormupTimeNow.is(':checked')) {
-//             pingText += '\n' + '**Formup Time:** NOW';
-//             webhookPingTextContent += '\n' + '**Formup Time:** NOW';
-//         } else {
-//             if (formupTime !== '') {
-//                 pingText += '\n' + '**Formup (EVE Time):** ' + formupTime;
-//                 webhookPingTextContent += '\n' + '**Formup (EVE Time):** ' + formupTime;
-//
-//                 // Get the timestamp and build the link to the timezones module if it's installed
-//                 if (fleetpingsSettings.timezonesInstalled === true) {
-//                     const timezonesUrl = getTimezonesUrl(formupTime);
-//
-//                     pingText += ' - ' + timezonesUrl;
-//
-//                     if (webhookType === 'Discord') {
-//                         webhookPingTextContent += ' ([Time Zone Conversion](' + timezonesUrl + '))';
-//                     }
-//
-//                     if (webhookType === 'Slack') {
-//                         webhookPingTextContent += ' (<' + timezonesUrl + '|Time Zone Conversion>)';
-//                     }
-//                 }
-//
-//                 const formupTimestamp = getFormupTimestamp(formupTime);
-//
-//                 pingText += '\n' + '**Formup (Local Time):** &lt;t:' + formupTimestamp + ':F&gt;';
-//                 webhookPingTextContent += '\n' + '**Formup (Local Time):** <t:' + formupTimestamp + ':F>';
-//             }
-//         }
-//
-//         // Check if fleet comms is available
-//         if (fleetComms !== '') {
-//             pingText += '\n' + '**Comms:** ' + fleetComms;
-//             webhookPingTextContent += '\n' + '**Comms:** ' + fleetComms;
-//         }
-//
-//         // Check if doctrine is available
-//         if (fleetDoctrine !== '') {
-//             pingText += '\n' + '**Ships / Doctrine:** ' + fleetDoctrine;
-//             webhookPingTextContent += '\n' + '**Ships / Doctrine:** ' + fleetDoctrine;
-//
-//             // Grab the doctrine link if there is one
-//             if (fleetDoctrineLink !== null) {
-//                 pingText += ' - ' + fleetDoctrineLink;
-//
-//                 if (webhookType === 'Discord') {
-//                     webhookPingTextContent += ' ([Doctrine Link](' + fleetDoctrineLink + '))';
-//                 }
-//
-//                 if (webhookType === 'Slack') {
-//                     webhookPingTextContent += ' (<' + fleetDoctrineLink + '|Doctrine Link>)';
-//                 }
-//             }
-//         }
-//
-//         // Check if srp is available
-//         if (fleetSrp !== '') {
-//             pingText += '\n' + '**SRP:** ' + fleetSrp;
-//             webhookPingTextContent += '\n' + '**SRP:** ' + fleetSrp;
-//
-//             if (fleetSrp === 'Yes' && fleetSrpCode !== '') {
-//                 pingText += ' (SRP Code: ' + fleetSrpCode + ')';
-//                 webhookPingTextContent += ' (SRP Code: ' + fleetSrpCode + ')';
-//             }
-//         }
-//
-//         // Check if additional information is available
-//         if (additionalInformation !== '') {
-//             pingText += '\n\n' + '**Additional Information**:' + '\n' + additionalInformation;
-//             webhookPingTextContent += '\n\n' + '**Additional Information**:' + '\n' + additionalInformation;
-//         }
-//
-//         if (webhookType === 'Discord') {
-//             $('.aa-fleetpings-ping-text').html(
-//                 '<p>' + nl2br(discordPingTarget + pingText, false) + '</p>'
-//             );
-//         }
-//
-//         if (webhookType === 'Slack') {
-//             $('.aa-fleetpings-ping-text').html(
-//                 '<p>' + nl2br(discordPingTarget + pingText.split('**').join('*'), false) + '</p>'
-//             );
-//         }
-//
-//         // Ping it directly if a webhook is selected
-//         if (webhookUrl !== '') {
-//             // add ping creator at the end
-//             if (fleetpingsSettings.pingCreator !== '') {
-//                 pingText += '\n\n' + '*(Ping sent by: ' + fleetpingsSettings.pingCreator + ')*';
-//                 webhookPingTextFooter = '(Ping sent by: ' + fleetpingsSettings.pingCreator + ')';
-//             }
-//
-//             // Default embed color
-//             let embedColor = '#faa61a';
-//
-//             if (fleetType !== '' && embedColor !== '') {
-//                 embedColor = webhookEmbedColor;
-//             }
-//
-//             // Send the ping to Discord
-//             if (webhookType === 'Discord') {
-//                 if ('undefined' !== webhookEmbedPing && webhookEmbedPing === 'True') {
-//                     if (pingTarget !== '') {
-//                         webhookPingTarget += ' :: ';
-//                     }
-//
-//                     sendEmbeddedDiscordPing(
-//                         webhookUrl,
-//                         webhookPingTarget + '**' + webhookPingTextHeader + '**' + '\n' + '** **',
-//                         {
-//                             'title': '**.: Fleet Details :.**',
-//                             'description': webhookPingTextContent,
-//                             'color': hexToDecimal(embedColor),
-//                             'footer': {
-//                                 'text': webhookPingTextFooter
-//                             }
-//                         }
-//                     );
-//                 } else {
-//                     sendDiscordPing(webhookUrl, webhookPingTarget + pingText);
-//                 }
-//             }
-//
-//             // Send the ping to Discord
-//             if (webhookType === 'Slack') {
-//                 let slackEmbedPingTarget = '';
-//
-//                 if (pingTarget !== '') {
-//                     slackEmbedPingTarget = '<' + webhookPingTarget.replace('@everyone', '!channel').replace('@', '!') + '> :: ';
-//                 }
-//
-//                 /**
-//                  * Payload to send to Slack
-//                  *
-//                  * @type {{attachments: [{color: string, footer: string, pretext: string, text: string, fallback: string}]}}
-//                  */
-//                 const payload = {
-//                     'attachments': [
-//                         {
-//                             'fallback': pingText,
-//                             'color': embedColor,
-//                             'pretext': slackEmbedPingTarget + '*' + webhookPingTextHeader + '*',
-//                             'text': '*.: Fleet Details :.*' + '\n' + webhookPingTextContent.split('**').join('*'),
-//                             'footer': webhookPingTextFooter
-// //                            'footer_icon': 'https://platform.slack-edge.com/img/default_application_icon.png'
-//                         }
-//                     ]
-//                 };
-//
-//                 sendSlackPing(webhookUrl, payload);
-//             }
-//
-//             // Tell the FC that it's already pinged
-//             showSuccess(
-//                 fleetpingsTranslations.ping.success,
-//                 '.aa-fleetpings-ping-copyresult'
-//             );
-//         }
-//     };
 
     /**
      * Copy the fleet ping to clipboard
@@ -652,6 +290,9 @@ $(document).ready(() => {
         $(inputFleetDoctrineUrl).val(fleetDoctrineLink);
     });
 
+    /**
+     * Set the webhook embed color
+     */
     $(selectFleetType).change(() => {
         const fleetTypeSelected = $('option:selected', selectFleetType);
         let webhookEmbedColor = null;
@@ -756,7 +397,7 @@ $(document).ready(() => {
     /**
      * Generate ping text
      */
-    $("form").submit((event) => {
+    $('form').submit((event) => {
         // Stop the browser from sending the form, we take care of it
         event.preventDefault();
 
@@ -766,8 +407,6 @@ $(document).ready(() => {
 
             return obj;
         }, {});
-
-        console.table(formData);
 
         $.ajax({
             url: fleetpingsSettings.url.fleetPing,

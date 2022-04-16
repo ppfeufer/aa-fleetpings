23c86ca3d19f4c632d5ffbe96648062e3202356b
Peter Pfeufer <info@ppfeufer.de>
2022-04-17 00:10:50 +0200

    [FIX] Unused variable 'test_has_access_with_allianceauth_optimer'

--- fleetpings/tests/test_access.py
+++ fleetpings/tests/test_access.py
@@ -194,34 +194,34 @@ class TestAccess(TestCase):
         self.assertEqual(response.context["fittings_installed"], False)
         self.assertEqual(response.context["srp_module_available_to_user"], False)
 
-        @modify_settings(
-            INSTALLED_APPS={
-                "remove": [
-                    "allianceauth.srp",
-                    "aasrp",
-                    "timezones",
-                    "allianceauth.optimer",
-                    "fittings",
-                ],
-                "append": "allianceauth.optimer",
-            }
-        )
-        def test_has_access_with_allianceauth_optimer(self):
-            """
-            Test that a user with access get to see it
-            :return:
-            """
+    @modify_settings(
+        INSTALLED_APPS={
+            "remove": [
+                "allianceauth.srp",
+                "aasrp",
+                "timezones",
+                "allianceauth.optimer",
+                "fittings",
+            ],
+            "append": "allianceauth.optimer",
+        }
+    )
+    def test_has_access_with_allianceauth_optimer(self):
+        """
+        Test that a user with access get to see it
+        :return:
+        """
 
-            # given
-            self.client.force_login(self.user_1003)
+        # given
+        self.client.force_login(self.user_1003)
 
-            # when
-            response = self.client.get(reverse("fleetpings:index"))
+        # when
+        response = self.client.get(reverse("fleetpings:index"))
 
-            # then
-            self.assertEqual(response.status_code, 200)
+        # then
+        self.assertEqual(response.status_code, 200)
 
-            # context data
-            self.assertEqual(response.context["optimer_installed"], True)
-            self.assertEqual(response.context["fittings_installed"], False)
-            self.assertEqual(response.context["srp_module_available_to_user"], False)
+        # context data
+        self.assertEqual(response.context["optimer_installed"], True)
+        self.assertEqual(response.context["fittings_installed"], False)
+        self.assertEqual(response.context["srp_module_available_to_user"], False)

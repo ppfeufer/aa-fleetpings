9a76c09a0f02f544acc8814905d589c8785a5f41
Peter Pfeufer <info@ppfeufer.de>
2023-04-25 21:43:16 +0200

    [CHANGE] Moved the build process to PEP 621 / pyproject.toml

--- fleetpings/__init__.py
+++ fleetpings/__init__.py
@@ -2,5 +2,10 @@
 A couple of variables to use throughout the app
 """
 
-__version__ = "2.20.0"
+# Standard Library
+from importlib import metadata
+
+__version__ = metadata.version("aa-fleetpings")
 __title__ = "Fleet Pings"
+
+del metadata

df1b9f91ec9b723ec76964761983d65069c7332e
Peter Pfeufer <info@ppfeufer.de>
2023-05-28 07:41:23 +0200

    [CHANGE] Make menu item translatable

--- fleetpings/__init__.py
+++ fleetpings/__init__.py
@@ -5,7 +5,10 @@ A couple of variables to use throughout the app
 # Standard Library
 from importlib import metadata
 
+# Django
+from django.utils.translation import gettext_lazy as _
+
 __version__ = metadata.version("aa-fleetpings")
-__title__ = "Fleet Pings"
+__title__ = _("Fleet Pings")
 
 del metadata

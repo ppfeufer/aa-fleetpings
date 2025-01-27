768f705551c8b931d7d174ed86763bd9927f76bc
Peter Pfeufer <info@ppfeufer.de>
2025-01-27 09:29:26 +0100

    [CHANGE] Use `django-sri` for sri hashes

--- fleetpings/templatetags/fleetpings.py
+++ fleetpings/templatetags/fleetpings.py
@@ -2,15 +2,27 @@
 Template tags
 """
 
+# Standard Library
+import os
+
 # Django
+from django.conf import settings
 from django.template.defaulttags import register
 from django.templatetags.static import static
+from django.utils.safestring import mark_safe
+
+# Alliance Auth
+from allianceauth.services.hooks import get_extension_logger
 
 # Alliance Auth (External Libs)
+from app_utils.logging import LoggerAddTag
 from app_utils.urls import reverse_absolute
 
 # AA Fleet Pings
-from fleetpings import __version__
+from fleetpings import __title__, __version__
+from fleetpings.helper.static_files import calculate_integrity_hash
+
+logger = LoggerAddTag(my_logger=get_extension_logger(__name__), prefix=__title__)
 
 
 @register.simple_tag
@@ -30,17 +42,57 @@ def fleetpings_reverse_url(view: str, *args) -> str:
 
 
 @register.simple_tag
-def fleetpings_static(path: str) -> str:
+def fleetpings_static(relative_file_path: str, script_type: str = None) -> str | None:
     """
-    Versioned Static URL
+    Versioned static URL
 
-    :param path:
-    :type path:
-    :return:
-    :rtype:
+    :param relative_file_path: The file path relative to the `aa-fleetpings/fleetpings/static/fleetpings` folder
+    :type relative_file_path: str
+    :param script_type: The script type
+    :type script_type: str
+    :return: Versioned static URL
+    :rtype: str
     """
 
-    static_url = static(path=path)
-    versioned_url = static_url + "?v=" + __version__
+    logger.debug(f"Getting versioned static URL for: {relative_file_path}")
 
-    return versioned_url
+    file_type = os.path.splitext(relative_file_path)[1][1:]
+
+    logger.debug(f"File extension: {file_type}")
+
+    # Only support CSS and JS files
+    if file_type not in ["css", "js"]:
+        raise ValueError(f"Unsupported file type: {file_type}")
+
+    static_file_path = os.path.join("fleetpings", relative_file_path)
+    static_url = static(static_file_path)
+
+    # Integrity hash calculation only for non-debug mode
+    sri_string = (
+        f' integrity="{calculate_integrity_hash(relative_file_path)}" crossorigin="anonymous"'
+        if not settings.DEBUG
+        else ""
+    )
+
+    # Versioned URL for CSS and JS files
+    # Add version query parameter to break browser caches when changing the app version
+    # Do not add version query parameter for libs as they are already versioned through their file path
+    versioned_url = (
+        static_url
+        if relative_file_path.startswith("libs/")
+        else static_url + "?v=" + __version__
+    )
+
+    # Return the versioned URL with integrity hash for CSS
+    if file_type == "css":
+        return mark_safe(f'<link rel="stylesheet" href="{versioned_url}"{sri_string}>')
+
+    # Return the versioned URL with integrity hash for JS files
+    if file_type == "js":
+        js_type = f' type="{script_type}"' if script_type else ""
+
+        return mark_safe(
+            f'<script{js_type} src="{versioned_url}"{sri_string}></script>'
+        )
+
+    return None
--- fleetpings/tests/test_templatetags.py
+++ fleetpings/tests/test_templatetags.py
@@ -4,23 +4,25 @@ Testing the template tags
 
 # Django
 from django.template import Context, Template
-from django.test import TestCase
+from django.test import TestCase, override_settings
 
 # Alliance Auth (External Libs)
 from app_utils.urls import site_absolute_url
 
 # AA Fleet Pings
 from fleetpings import __version__
+from fleetpings.helper.static_files import calculate_integrity_hash
 
 
 class TestVersionedStatic(TestCase):
     """
-    Test fleetpings_versioned_static template tag
+    Test the `fleetpings_static` template tag
     """
 
+    @override_settings(DEBUG=False)
     def test_versioned_static(self):
         """
-        Test should return a versioned static URL
+        Test should return the versioned static
 
         :return:
         :rtype:
@@ -30,16 +32,60 @@ class TestVersionedStatic(TestCase):
         template_to_render = Template(
             template_string=(
                 "{% load fleetpings %}"
-                "{% fleetpings_static 'fleetpings/css/fleetpings.min.css' %}"
+                "{% fleetpings_static 'css/fleetpings.min.css' %}"
+                "{% fleetpings_static 'js/fleetpings.min.js' 'module' %}"
             )
         )
 
         rendered_template = template_to_render.render(context=context)
 
-        self.assertInHTML(
-            needle=f'/static/fleetpings/css/fleetpings.min.css?v={context["version"]}',
-            haystack=rendered_template,
+        expected_static_css_src = (
+            f'/static/fleetpings/css/fleetpings.min.css?v={context["version"]}'
         )
+        expected_static_css_src_integrity = calculate_integrity_hash(
+            "css/fleetpings.min.css"
+        )
+        expected_static_js_src = (
+            f'/static/fleetpings/js/fleetpings.min.js?v={context["version"]}'
+        )
+        expected_static_js_src_integrity = calculate_integrity_hash(
+            "js/fleetpings.min.js"
+        )
+
+        self.assertIn(member=expected_static_css_src, container=rendered_template)
+        self.assertIn(
+            member=expected_static_css_src_integrity, container=rendered_template
+        )
+        self.assertIn(member=expected_static_js_src, container=rendered_template)
+        self.assertIn(
+            member=expected_static_js_src_integrity, container=rendered_template
+        )
+
+    @override_settings(DEBUG=True)
+    def test_versioned_static_with_debug_enabled(self) -> None:
+        """
+        Test versioned static template tag with DEBUG enabled
+
+        :return:
+        :rtype:
+        """
+
+        context = Context({"version": __version__})
+        template_to_render = Template(
+            template_string=(
+                "{% load fleetpings %}"
+                "{% fleetpings_static 'css/fleetpings.min.css' %}"
+            )
+        )
+
+        rendered_template = template_to_render.render(context=context)
+
+        expected_static_css_src = (
+            f'/static/fleetpings/css/fleetpings.min.css?v={context["version"]}'
+        )
+
+        self.assertIn(member=expected_static_css_src, container=rendered_template)
+        self.assertNotIn(member="integrity=", container=rendered_template)
 
 
 class TestReverseUrl(TestCase):

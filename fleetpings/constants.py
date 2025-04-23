028671efa0aacc08dbc986196c42b7b874bd4f32
Peter Pfeufer <info@ppfeufer.de>
2025-04-23 17:00:48 +0200

    [ADD] Prefix for internal URLs

--- fleetpings/constants.py
+++ fleetpings/constants.py
@@ -11,5 +11,8 @@ GITHUB_URL = f"https://github.com/ppfeufer/{APP_NAME}"
 
 DISCORD_WEBHOOK_REGEX = r"https:\/\/discord\.com\/api\/webhooks\/[\d]+\/[a-zA-Z0-9_-]+$"
 
+# All internal URLs need to start with this prefix
+INTERNAL_URL_PREFIX = "-"
+
 APP_BASE_DIR = os.path.join(os.path.dirname(__file__))
 APP_STATIC_DIR = os.path.join(APP_BASE_DIR, "static", PACKAGE_NAME)

8a0f7c0d87d493359331d7f85b7f885dc93aa68c
Peter Pfeufer <info@ppfeufer.de>
2025-07-08 11:12:30 +0200

    [REMOVE] Unused constants

--- fleetpings/constants.py
+++ fleetpings/constants.py
@@ -2,9 +2,6 @@
 Constants
 """
 
-# Standard Library
-import os
-
 APP_NAME = "aa-fleetpings"
 PACKAGE_NAME = "fleetpings"
 GITHUB_URL = f"https://github.com/ppfeufer/{APP_NAME}"
@@ -13,6 +10,3 @@ DISCORD_WEBHOOK_REGEX = r"https:\/\/discord\.com\/api\/webhooks\/[\d]+\/[a-zA-Z0
 
 # All internal URLs need to start with this prefix
 INTERNAL_URL_PREFIX = "-"
-
-APP_BASE_DIR = os.path.join(os.path.dirname(__file__))
-APP_STATIC_DIR = os.path.join(APP_BASE_DIR, "static", PACKAGE_NAME)

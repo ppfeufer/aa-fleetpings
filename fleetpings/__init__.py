507ccc7c041284455bdf8b756715a80821e6d2f3
Peter Pfeufer <info@ppfeufer.de>
2025-08-07 17:37:16 +0200

    [RELEASE] v3.7.1
    ## [3.7.1] - 2025-08-07
    
    ### Changed
    
    - Fetch API functions refactored

--- fleetpings/__init__.py
+++ fleetpings/__init__.py
@@ -6,7 +6,7 @@ A couple of variables to use throughout the app
 # Django
 from django.utils.translation import gettext_lazy as _
 
-__version__ = "3.7.0"
+__version__ = "3.7.1"
 __title__ = _("Fleet Pings")
 
 __package_name__ = "aa-fleetpings"

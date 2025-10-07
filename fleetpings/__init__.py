410052edef0a308e24cb4d661bc5f2e142d01339
Peter Pfeufer <info@ppfeufer.de>
2025-10-07 08:24:02 +0200

    [RELEASE] v3.8.2
    ## [3.8.2] - 2025-10-07
    
    ### Changed
    
    - Translations updated

--- fleetpings/__init__.py
+++ fleetpings/__init__.py
@@ -6,7 +6,7 @@ A couple of variables to use throughout the app
 # Django
 from django.utils.translation import gettext_lazy as _
 
-__version__ = "3.8.1"
+__version__ = "3.8.2"
 __title__ = _("Fleet Pings")
 
 __package_name__ = "aa-fleetpings"
